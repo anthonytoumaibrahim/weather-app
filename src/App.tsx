ae2ea0e9db42436ba8fed9aed892973d355181e3
atian155 <atianthony@outlook.com>
2024-05-24 08:53:59 +0300

    Add Main App Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Box, Typography } from "@mui/material";
 import "./App.css";
+import Container from "@mui/material/Container";
 
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <Container maxWidth="md">
+      <Box padding={4}>
+        <Typography variant="h3" component="h3" align="center">
+          Welcome to {process.env.REACT_APP_NAME}!
+        </Typography>
+      </Box>
+    </Container>
+  );
 };
 
 export default App;
